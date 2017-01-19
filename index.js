149b7e036668c0e06e039263b6dd753d0f55807a
Alabê Duarte <alabeduarte@gmail.com>
2017-01-18 22:39:39 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {ProgressBarsContainer} from './src/components';
+
+ReactDOM.render(<ProgressBarsContainer/>, document.querySelector('#root'));
---
+++ src/components.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class ProgressBarsContainer extends Component {
+  render() {
+    return <h1>Progress Bars</h1>
+  }
+}
